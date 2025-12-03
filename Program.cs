9e0a7e5b1875cc074258c27185c897ae50cf114e
protodeleter <pavel.nsnv@gmail.com>
2025-12-03 12:48:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace QaAuto
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task_6 tsk6 = new Task_6();
+
+            //Console.WriteLine(tsk6.perfectNumber(25));
+
+
+        }
+
+        
+    }
+}
---
+++ Task_6.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSix
+{
+    public class Task_6
+    {
+        
+        public bool perfectNumber(int numb)
+        {
+
+            int res = 0;
+            int sum = 0;
+            int temp = numb;
+
+            while (temp > 0)
+            {
+                res = temp % 10;
+                temp = temp / 10;
+
+                sum = sum + res;
+            }
+
+
+            if (numb % sum == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+}
